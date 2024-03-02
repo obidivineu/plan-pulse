d8ac936ccfe961b0e7603497117c99d1422713e5
myKey <divineobi1250@gmail.com>
2024-03-02 23:50:03 +0100

    build

--- src/Pages/landing-page/Header/Header.jsx
+++ src/Pages/landing-page/Header/Header.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import logo from "../../../assets/gymlogo.png"
 import "./Header.css"
 import { useNavigate } from 'react-router-dom';
+import { Link as ScrollLink } from 'react-scroll';
+
 
 
 const Header = () => {
   const nav = useNavigate()
 
-  const navToSignUp = () =>{
-    nav("/about-page")
-  }
   
-  const navHomeScreen = () =>{
+
+  const navHomeScreen = () => {
     nav("/home-screen")
   }
 
@@ -23,15 +23,42 @@ const Header = () => {
         <img src={logo} alt="logo" />
       </div>
       <div className="headertext">
-        <p className="home"  style={{ fontSize: "15px"}} onClick={navHomeScreen}>HOME</p>
-        <p className="home"  style={{ fontSize: "15px"}}   onClick={navToSignUp} >ABOUT</p>
-        <p className="home"  style={{ fontSize: "15px"}}>FEATURES</p>
-        <p className="home"  style={{ fontSize: "15px"}}>PRICING</p>
-        <p className="home"  style={{ fontSize: "15px"}}>SERVICE</p>
+        <p className="home" style={{ fontSize: "15px" }} onClick={navHomeScreen}>HOME
+        </p>
+        <p className="home" style={{ fontSize: "15px" }}>
+        <ScrollLink 
+        activeClass='active-scroll-link'
+        to='FEATURES'
+        spy={true}
+        smooth={true}
+        offset={-100}
+        duration={500}>FEATURES</ScrollLink></p>
+        <p className="home" style={{ fontSize: "15px" }}> <ScrollLink 
+        activeClass='active-scroll-link'
+        to='PRICING'
+        spy={true}
+        smooth={true}
+        offset={-100}
+        duration={500}>PRICING</ScrollLink></p>
+        <p className="home" style={{ fontSize: "15px" }}><ScrollLink 
+        activeClass='active-scroll-link'
+        to='SERVICES'
+        spy={true}
+        smooth={true}
+        offset={-100}
+        duration={500}>SERVICES</ScrollLink></p>
+         <p className="home" style={{ fontSize: "15px" }} ><ScrollLink 
+        activeClass='active-scroll-link'
+        to='ABOUT'
+        spy={true}
+        smooth={true}
+        offset={-100}
+        duration={500}>ABOUT US</ScrollLink></p>
       </div>
       <div className="started">
-        <button className="get" onClick={()=>{
-          nav('/user-login')}} style={{ fontSize: "15px"}}>LOGIN</button>
+        <button className="get" onClick={() => {
+          nav('/user-login')
+        }} style={{ fontSize: "15px" }}>LOGIN</button>
       </div>
     </div>
 
